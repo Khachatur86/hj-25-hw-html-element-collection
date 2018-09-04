95f71c32ee40b23ef1214188aba4b122947bf095
Хачатур <>
2018-09-04 22:14:30 +0300

    Second correct

--- audioplayer/js/audio.js
+++ audioplayer/js/audio.js
@@ -1,66 +1,60 @@
 //Pseudocode
 
-// Play
+// Let's find the buttons Play, Stop, Pause, Next, Back
 
-// Pause
+(function() {
+  // Let's find the buttons Play, Stop, Pause, Next, Back
+  const player = document.getElementsByClassName('mediaplayer')[0],
+        audio = player.getElementsByTagName('audio')[0],
+        title = player.getElementsByClassName('title')[0],
+        playPauseBtn = player.getElementsByClassName('playstate')[0],
+        stopBtn = player.getElementsByClassName('stop')[0],
+        nextBtn = player.getElementsByClassName('next')[0],
+        backBtn = player.getElementsByClassName('back')[0],
+        songsList = ['LA Chill Tour.mp3', 
+                 'LA Fusion Jam.mp3', 
+                 'This is it band.mp3'];
 
-// Stop
+  
+  let songIndex = 0;
 
-// Back
-
-// Next
-
-(function () {
-
-  const player = document.getElementsByClassName('mediaplayer')[0];
-  const audio = player.getElementsByTagName('audio')[0];
-  const title = player.getElementsByClassName('title')[0];
-  const playPauseBtn = player.getElementsByClassName('playstate')[0];
-  const stopBtn = player.getElementsByClassName('stop')[0];
-  const prevBtn = player.getElementsByClassName('back')[0];
-  const nextBtn = player.getElementsByClassName('next')[0];
-  const trackList = ['LA Chill Tour', 'LA Fusion Jam', 'This is it band'];
-  let trackIndex = 0;
-
-  function setCurrentTrack() {
-    audio.src = `./mp3/${trackList[trackIndex]}.mp3`;
-    title.title = trackList[trackIndex];
-
+  // Set current song
+  function setCurrentSong() {
+    audio.src = `./mp3/${songsList[songIndex]}`;
+    //add atribute appropriate title
+    title.title = songsList[songIndex];
     if (player.classList.contains('play')) {
-      audio.play()
-    }
-  }
-
-  playPauseBtn.onclick = () => {
-    player.classList.contains('play') ? audio.pause() : audio.play();
-    player.classList.toggle('play');
+        audio.play();
+    };
   };
+
+  // playPause button function realisation 
+  playPauseBtn.onclick = () => {
+    if (player.classList.contains('play')) {
+      audio.pause();}
+      else {
+        audio.play();
+      };
+      player.classList.toggle('play');
+  };
+
+  backBtn.onclick = () => {
+    
+    setCurrentSong();
+  };
+
+
 
   stopBtn.onclick = () => {
-    if (player.classList.contains('play')) {
       player.classList.remove('play');
-    }
-
-    audio.pause();
-    audio.currentTime = 0;
+      audio.pause();
+      audio.currentTime = 0;
   };
-
-  prevBtn.onclick = () => {
-    if (trackIndex <= 0) {
-      trackIndex = trackList.length - 1;
-    } else {
-      trackIndex--;
-    }
-    setCurrentTrack();
-  }
-
+   
   nextBtn.onclick = () => {
-    if (trackIndex >= trackList.length - 1) {
-      trackIndex = 0;
-    } else {
-      trackIndex++;
-    }
-    setCurrentTrack();
-  }
+    
+    setCurrentSong();
+  };
+  
+})()
 
-})();
