2311af40c7a30550598529e1c90deda807e66105
Khachatur86 <35083057+Khachatur86@users.noreply.github.com>
2018-09-06 12:05:12 +0300

    Update audio.js
    Adding function for Next and Back buttons.

--- audioplayer/js/audio.js
+++ audioplayer/js/audio.js
@@ -19,6 +19,7 @@
   let songIndex = 0;
 
   // Set current song
+  
   function setCurrentSong() {
     audio.src = `./mp3/${songsList[songIndex]}`;
     //add atribute appropriate title
@@ -37,22 +38,33 @@
       };
       player.classList.toggle('play');
   };
-
+  
+  // Back Button function realisation
+  
   backBtn.onclick = () => {
-    
+    if (songIndex <= 0) {
+      songIndex = 0;
+     } else {
+     songIndex--}
     setCurrentSong();
   };
 
 
-
+  // Stop Button functipn realisation
+  
   stopBtn.onclick = () => {
       player.classList.remove('play');
       audio.pause();
       audio.currentTime = 0;
   };
-   
+  
+  // Next Button function realisation
+  
   nextBtn.onclick = () => {
-    
+    if (songIndex >= songsList.length - 1) { // (songIndex == songsList.length - 1) this condition enough
+      songIndex = 0;
+    } else {
+      songIndex++} 
     setCurrentSong();
   };
   
